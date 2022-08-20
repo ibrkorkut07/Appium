c8dad6673505eb96a917af3a9b99074cf65aa56f
ibrkorkut07 <ibr.kor@gmail.com>
2022-08-20 16:26:52 +0200

    app 4

--- src/test/java/Appium/Appium03.java
+++ src/test/java/Appium/Appium03.java
@@ -1,6 +1,5 @@
 package Appium;
 
-
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
